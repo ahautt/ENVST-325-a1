65e8dbcf494311674ec78ca1572d4b3630a74df2
ahautt <ahaut@hamilton.edu>
2026-01-29 16:31:56 +0000

    description of changes

---
+++ A1.R
@@ -0,0 +1,39 @@
+# in class prompts ----
+
+# creating a variable 
+
+variable_1 <- 50
+variable_1*50
+
+# creating vector's
+
+peaks <- c(5344,5114,4960)
+
+prom <- c(4914,2100,840)
+
+peaknames <- c("Mount Marcy","Algonquin Peak",
+              "Mount Haystack")
+
+# creating a data frame
+
+highPeaks <- data.frame(elev = peaks,
+                        prom = prom,
+                        name = peaknames)
+
+# shows a column
+highPeaks$elev
+
+#shows a object
+highPeaks[1,1]
+
+#shows a row
+highPeaks[1,]
+
+## prompt 1
+
+
+
+# homework --------
+
+
+
