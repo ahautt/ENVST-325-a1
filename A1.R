dcffe9ed0c7f99c0fbf2fcae5b609eb252ced3ba
ahautt <ahaut@hamilton.edu>
2026-01-29 16:44:24 +0000

    class prompts

--- A1.R
+++ A1.R
@@ -31,7 +31,25 @@ highPeaks[1,]
 
 ## prompt 1
 
+avg_snow_depth_inches <- c(2.5,3,5,4.5)
+avg_snow_depth_cent <- avg_snow_depth_inches*2.54
 
+## promt 2
+
+peaknames <- c("Mount Haystack", "Mount Skylight", "Dix Mountain", "Gray Peak")
+elev_ft <- c(4960,4926,4857,4840)
+ascent_ft <- c(3570,4265,2800,4178)
+rt_length <- c(17.8,17.9,13.2,16)
+
+peak_summits <- data.frame(Name = peaknames,
+                           Elavation = elev_ft,
+                           ClimbAscent = ascent_ft,
+                           RoundTripLength = rt_length)
+
+## prompt 3
+
+temps <- c(-44,0,20,35)
+temps_F <- temps*32
 
 # homework --------
 
